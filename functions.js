1d2b891c1783f70376a58b9190b4f0f973f1f78d
Thiago Sisaque <tsisaque08.dev@gmail.com>
2022-03-11 22:56:49 -0300

    rotas de adicionar e deletar produtos foram adicionadas

--- functions.js
+++ functions.js
@@ -1,15 +1,51 @@
-
 const fs = require('fs');
 
-function get_products() {
 
+function get_products() {
     let rawdata = fs.readFileSync('database.json');
     let products = JSON.parse(rawdata);
-    
+
     return products;
+}
+
+function add_product(product){
+    let rawdata = fs.readFileSync('database.json');
+    let database = JSON.parse(rawdata);
+
+    database.products.push(product);
+
+    fs.writeFile("database.json", JSON.stringify(database), 'utf8', function (err) {
+        if (err) {
+            console.log("Um erro aconteceu ao escrever no arquivo.");
+            return console.log(err);
+        }
+     
+        console.log("Produto adicionado.");
+    });
 
 }
 
+
+function delete_product(product_id){
+    let rawdata = fs.readFileSync('database.json');
+    let database = JSON.parse(rawdata);
+
+    database.products = database.products.filter(({ id }) => id !== product_id);
+
+    fs.writeFile("database.json", JSON.stringify(database), 'utf8', function (err) {
+        if (err) {
+            console.log("Um erro aconteceu ao escrever no arquivo.");
+            return console.log(err);
+        }
+     
+        console.log("Produto removido.");
+    });
+
+}
+
+
 module.exports = {
-    get_products
+    get_products,
+    add_product,
+    delete_product
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { get_products } = require('./functions.js');
+const { products } = require('../delivery-mqtt/backend/db.js');
+const { get_products, add_product, delete_product } = require('./functions.js');
 
 
 const app = express();
@@ -8,13 +9,35 @@ app.use(express.json());
 
 
 app.get("/ler_produtos", function(req, res) {
-    products = get_products();
+    let products = get_products();
     res.send(products);
 });
 
+
 app.post("/adicionar_produto", function(req, res) {
+    
     const { id, name, price, category } = req.body;
-    console.log(id, name, price, category);
+
+    let product = {
+        id,
+        name,
+        price,
+        category
+    }
+
+    add_product(product);
+
+    res.send("Produto adicionado com sucesso!");
+});
+
+
+app.delete("/remover_produto", function(req, res) {
+    
+    const { id } = req.body;
+
+    delete_product(id);
+
+    res.send("Produto removido com sucesso!");
 });
 
 

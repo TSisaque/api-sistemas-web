6b7ed04e6533373c76deb47467e84f0dda716d66
Thiago Sisaque <tsisaque08.dev@gmail.com>
2022-03-11 22:05:05 -0300

    rota ler produtos adicionada

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+
 const app = express();
+
+
+app.get("/ler_produtos", function (req, res) {
+    res.send("oi");
+});
+
 
 app.listen(3000, function () {
     console.log('Node app está funcionando!');
